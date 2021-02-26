1cc41af1c7a683985499413ab71b2fb561033b87
MaxKharena <m.harena@asd-team.com>
2021-02-26 16:33:49 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Routes from './Routes';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import './fonts.css';
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
