74d01f7368d263abea540721f129acf125f57500
MaxKharena <m.harena@asd-team.com>
2021-02-26 16:18:41 +0200

    fix

--- src/Components/404/index.js
+++ src/Components/404/index.js
@@ -1,5 +1,12 @@
+
+// Absolute imports
 import React from 'react';
 
-export default () => (
+// Layout
+import Layout from '../Layout';
+
+const Page404 = () => (
     <h1>404</h1>
 );
+
+export default Layout()(Page404);
